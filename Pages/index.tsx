355b0bf10a83f1d0a51997dc0d794a7c4c07e8fd
sb27v7g4yn-hash <sb27v7g4yn@privaterelay.appleid.com>
2025-11-05 10:42:34 -0800

    Create index.tsx

---
+++ Pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>NFL Betting Analyzer</h1>
+      <p>This is your private dashboard. Week 1 picks coming soon.</p>
+    </main>
+  );
+}
